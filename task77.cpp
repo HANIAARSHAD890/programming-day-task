5855e07ccbbaa2f0d1eefbb2c7efd8cc63ac180b
Hania Arshad <144407628+HANIAARSHAD890@users.noreply.github.com>
2023-09-17 21:00:45 +0500

    Add files via upload

---
+++ task77.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+main() 
+{ 
+        cout << "     ~^^^~        _ ()_  "<<endl;
+        cout << "     ^3 3^       | 3 3 |       "<<endl;
+        cout << "     |___|       |_____|      "<<endl;
+        cout<<  "      | |          | |     "<<endl;
+        cout<<  "    fire boy      water girl"<<endl;
+            
+}    
